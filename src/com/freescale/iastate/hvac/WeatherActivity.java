0700cf8a01d5997665203a72711f38009c6a0e08
tikihead <james.a.rich@gmail.com>
2012-09-09 21:11:19 -0500

    woo! weather is back! using fragments now.

--- src/com/freescale/iastate/hvac/WeatherActivity.java
+++ src/com/freescale/iastate/hvac/WeatherActivity.java
@@ -18,10 +18,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.freescale.iastate.hvac.weather.JSONParser;
+import com.freescale.iastate.hvac.weather.WeatherFragment;
 import com.freescale.iastate.hvac.weather.WeatherObject;
 
 public class WeatherActivity extends Activity implements MenuInterface {
 	final char degree = 0x00B0;
+	WeatherObject[] dayArray = new WeatherObject[12];
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,9 +33,9 @@ public class WeatherActivity extends Activity implements MenuInterface {
 
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		
-		//this sets the help string for the current activity.
-		//copy paste 
+
+		// this sets the help string for the current activity.
+		// copy paste
 		rootIntent.setHelpText(getText(R.string.weather_help));
 
 		ForecastWeatherTask task = new ForecastWeatherTask();
@@ -42,57 +44,43 @@ public class WeatherActivity extends Activity implements MenuInterface {
 	}
 
 	private class ForecastWeatherTask extends AsyncTask<Void, Void, String> {
-		ImageView dayOneImageView = (ImageView) findViewById(R.id.day1image);
-		ImageView dayTwoImageView = (ImageView) findViewById(R.id.day2image);
-		ImageView dayThreeImageView = (ImageView) findViewById(R.id.day3image);
-		TextView dayOneTextView = (TextView) findViewById(R.id.day1text);
-		TextView dayTwoTextView = (TextView) findViewById(R.id.day2text);
-		TextView dayThreeTextView = (TextView) findViewById(R.id.day3text);
 
 		String day1Text, day2Text, day3Text;
 		Drawable day1Image, day2Image, day3Image;
 
 		protected String doInBackground(Void... params) {
 			try {
-				
+
 				SharedPreferences settings = PreferenceManager
 						.getDefaultSharedPreferences(getBaseContext());
 				String zip = settings.getString("zip_code_key", "50014");
 
 				JSONParser parser = new JSONParser();
 				JSONParser.zip = zip;
-				JSONObject json = parser
-						.getJSONFromUrl(JSONParser.threedayURL);
+				JSONObject json = parser.getJSONFromUrl(JSONParser.threedayURL);
 				String readable = json.toString(5);
-				
+
 				JSONObject forecast = json.getJSONObject("forecast");
-				JSONObject simpleForecast = forecast.getJSONObject("simpleforecast");
+				JSONObject simpleForecast = forecast
+						.getJSONObject("simpleforecast");
 				JSONArray forecastDay = null;
 				forecastDay = simpleForecast.getJSONArray("forecastday");
+
 				
-				WeatherObject[] dayArray = new WeatherObject [12];
-				for (int i=0; i<forecastDay.length(); i++){
+				for (int i = 0; i < forecastDay.length(); i++) {
 					JSONObject day = forecastDay.getJSONObject(i);
 					JSONObject date = day.getJSONObject("date");
 					JSONObject high = day.getJSONObject("high");
 					JSONObject low = day.getJSONObject("low");
-				//	String readable2 = day.toString(5);
+					// String readable2 = day.toString(5);
 					dayArray[i] = new WeatherObject();
 					dayArray[i].weekday = date.getString("weekday");
 					dayArray[i].high = high.getString("fahrenheit");
 					dayArray[i].low = low.getString("fahrenheit");
 					dayArray[i].conditions = day.getString("conditions");
+					dayArray[i].image = weatherImage(day.getString("conditions"));
 				}
-				
-				
-				day1Text = dayArray[0].conditions;
-				day1Image = weatherImage(day1Text);
 
-				day2Text = dayArray[1].conditions;
-				day2Image = weatherImage(day2Text);
-
-				day3Text = dayArray[2].conditions;
-				day3Image = weatherImage(day3Text);
 				return null;
 
 			} catch (Exception e) {
@@ -103,12 +91,20 @@ public class WeatherActivity extends Activity implements MenuInterface {
 		}
 
 		protected void onPostExecute(String result) {
-			dayOneTextView.setText(day1Text);
-			dayOneImageView.setImageDrawable(day1Image);
-			dayTwoTextView.setText(day2Text);
-			dayTwoImageView.setImageDrawable(day2Image);
-			dayThreeTextView.setText(day3Text);
-			dayThreeImageView.setImageDrawable(day3Image);
+
+			
+			WeatherFragment day1 = (WeatherFragment) getFragmentManager().findFragmentById(
+					R.id.day1fragment);
+			day1.updateWeather(dayArray[0]);
+			
+			WeatherFragment frag2 = (WeatherFragment) getFragmentManager().findFragmentById(
+					R.id.day2fragment);
+			frag2.updateWeather(dayArray[1]);		
+
+			WeatherFragment frag3 = (WeatherFragment) getFragmentManager().findFragmentById(
+					R.id.day3fragment);
+			frag3.updateWeather(dayArray[2]);
+
 		}
 
 	}
@@ -120,7 +116,7 @@ public class WeatherActivity extends Activity implements MenuInterface {
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
-			return rootIntent.onOptionsItemSelected(this, item);
+		return rootIntent.onOptionsItemSelected(this, item);
 	}
 
 	public Drawable weatherImage(String condition) {
@@ -146,7 +142,5 @@ public class WeatherActivity extends Activity implements MenuInterface {
 		return conditionDrawable;
 
 	}
-	
-
 
 }
--- src/com/freescale/iastate/hvac/weather/WeatherObject.java
+++ src/com/freescale/iastate/hvac/weather/WeatherObject.java
@@ -1,10 +1,13 @@
 package com.freescale.iastate.hvac.weather;
 
+import android.graphics.drawable.Drawable;
+
 public class WeatherObject {
 	public String weekday;
 	public String high;
 	public String low;
 	public String conditions;
+	public Drawable image;
 	
 
 	
