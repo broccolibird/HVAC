0fc6de403e860488b482748c1b0ece541cd30f89
Namara <tamjbart@gmail.com>
2012-11-13 00:08:06 -0600

    Adds graphs that switch on button press with generated data

--- src/com/freescale/iastate/hvac/energy/GraphFragment.java
+++ src/com/freescale/iastate/hvac/energy/GraphFragment.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.freescale.iastate.hvac.energy;
 
 /**
@@ -31,17 +28,12 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-/**
- * @author Namara
- * 
- */
 public class GraphFragment extends Fragment {
-	final int DAY = 1;//24;
-	final int WEEK = 2;//7;
-	final int MONTH = 3;//30;
-	final int YEAR = 4;//12;
-	final private String seriesTitle = "Number of kilowatt hours";
-	
+	final int DAY = 1;
+	final int WEEK = 2;
+	final int MONTH = 3;
+	final int YEAR = 4;
+
 	final int MAXKWH = 100;
 
 	LinearLayout graphLayout;
@@ -59,7 +51,7 @@ public class GraphFragment extends Fragment {
 		localInflater = inflater;
 		localContainer = container;
 		view = inflater.inflate(R.layout.graphs_fragment, container, false);
-		
+
 		timePeriod = WEEK;
 		barColor = Color.WHITE;
 		axesColor = Color.WHITE;
@@ -82,29 +74,40 @@ public class GraphFragment extends Fragment {
 	 */
 	private XYMultipleSeriesDataset getGeneratedDataset(int timePeriod) {
 		final int floor = 65;
-		final int variation = 17;
-		
+		int averageKWH = 0;
+		int variation = 0;
+
 		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
 		Calendar c = Calendar.getInstance();
 		int numDataPoints = 0;
-		
-		if (timePeriod == DAY){
+
+		if (timePeriod == DAY) {
+			averageKWH = hourKWH();
+			variation = 4;
 			c = Calendar.getInstance();
-			numDataPoints = c.get(Calendar.HOUR_OF_DAY)+1;
-		} else if (timePeriod == WEEK){
+			numDataPoints = c.get(Calendar.HOUR_OF_DAY) + 1;
+		} else if (timePeriod == WEEK) {
+			averageKWH = dayKWH();
+			variation = 10;
 			c = Calendar.getInstance();
-			numDataPoints = c.get(Calendar.DAY_OF_WEEK)+1;
-		} else if (timePeriod == MONTH){
+			numDataPoints = c.get(Calendar.DAY_OF_WEEK) + 1;
+		} else if (timePeriod == MONTH) {
+			averageKWH = dayKWH();
+			variation = 10;
 			c = Calendar.getInstance();
-			numDataPoints = c.get(Calendar.DATE)+1;
-		} else if (timePeriod == YEAR){
+			numDataPoints = c.get(Calendar.DATE) + 1;
+		} else if (timePeriod == YEAR) {
+			variation = 15;
+			averageKWH = monthKWH();
 			numDataPoints = 12;
-		} 
-		
+		}
+
+
+		final String seriesTitle = "Number of kilowatt hours";
 		XYSeries series = new XYSeries(seriesTitle);
 		for (int k = 1; k < numDataPoints; k++) {
 			Double rando = Math.random() * variation;
-			int kWattHr = (int) (Math.floor(rando) + floor);
+			int kWattHr = (int) (Math.floor(rando) + averageKWH);
 			series.add(k, kWattHr);
 		}
 		dataset.addSeries(series);
@@ -115,62 +118,70 @@ public class GraphFragment extends Fragment {
 	private XYMultipleSeriesRenderer getChartRenderer(int timePeriod) {
 		ChartSettings s = new ChartSettings();
 		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-		
+
 		Calendar c = Calendar.getInstance();
 		String chartTitle = "Demo Chart";
 		String xTitle = "";
 		double numBars = 0;
 		double xMin = 0.5;
-		
-		if (timePeriod == DAY){
+		double yMax = 0;
+
+		if (timePeriod == DAY) {
 			chartTitle = "Energy Usage Today";
 			xTitle = "Hours";
 			numBars = 24.5;
-			
+			yMax = hourKWH() +  hourKWH()*.4;
+
 			renderer.clearXTextLabels();
 
-			String[] hours = {"", "3 AM", "6 AM", "9 AM", "12 PM", "3 PM", "6 PM", "9 PM", "12 AM"};
-			
-			for(int i = 0; i < 9; i++){
-				renderer.addXTextLabel((double)i*3, hours[i]);
+			String[] hours = { "", "3 AM", "6 AM", "9 AM", "12 PM", "3 PM",
+					"6 PM", "9 PM", "12 AM" };
+
+			for (int i = 0; i < 9; i++) {
+				renderer.addXTextLabel((double) i * 3, hours[i]);
 			}
 			renderer.setXLabels(0);
-		} else if (timePeriod == WEEK){
+		} else if (timePeriod == WEEK) {
 			chartTitle = "Energy Usage This Week";
 			xTitle = "Days";
 			numBars = 7.5;
+			yMax = dayKWH() +  dayKWH()*.4;
 			renderer.clearXTextLabels();
 
-			String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-			
-			for(int i = 0; i < 7; i ++){
+			String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday",
+					"Friday", "Saturday", "Sunday" };
+
+			for (int i = 0; i < 7; i++) {
 				double d = i + 1;
 				renderer.addXTextLabel(d, days[i]);
 			}
 			renderer.setXLabels(0);
-		} else if (timePeriod == MONTH){
+		} else if (timePeriod == MONTH) {
 			chartTitle = "Energy Usage This Month";
 			xTitle = "Days";
-			numBars = c.getActualMaximum(Calendar.DAY_OF_MONTH)+1;
+			numBars = c.getActualMaximum(Calendar.DAY_OF_MONTH) + 1;
+			yMax = dayKWH() +  dayKWH()*.4;
 			renderer.setXLabels((int) numBars);
-		} else if (timePeriod == YEAR){
+		} else if (timePeriod == YEAR) {
 			chartTitle = "Energy Usage This Year";
 			xTitle = "Months";
+			yMax = monthKWH() +  monthKWH()*.4;
 			numBars = 12.5;
-			
+
 			renderer.clearXTextLabels();
 
-			String[] days = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
-			
-			for(int i = 0; i < 12; i ++){
+			String[] days = { "Jan", "Feb", "March", "April", "May", "June",
+					"July", "Aug", "Sept", "Oct", "Nov", "Dec" };
+
+			for (int i = 0; i < 12; i++) {
 				double d = i + 1;
 				renderer.addXTextLabel(d, days[i]);
 			}
 			renderer.setXLabels(0);
-		} 
-		
-		s.setChartSettings(renderer, chartTitle, xTitle, "kWh", xMin,
-				numBars, 0, MAXKWH, axesColor, fontColor);
+		}
+
+		s.setChartSettings(renderer, chartTitle, xTitle, "kWh", xMin, numBars,
+				0, yMax, axesColor, fontColor);
 		s.setTextSize(renderer, 20, 40, 20);
 		s.setAutoSpacing(renderer);
 
@@ -181,22 +192,48 @@ public class GraphFragment extends Fragment {
 		renderer.setShowLegend(false);
 		renderer.addSeriesRenderer(r);
 		return renderer;
+
+	}
+	
+	private int monthKWH(){
+		int avgKWHperYear = 14000;
+		int monthKWH = avgKWHperYear/12;
+		return monthKWH;
+	}
+	
+	private int weekKWH(){
+		int avgKWHperYear = 14000;
+		int weekKWH = avgKWHperYear/52;
+		return weekKWH;
+	}
+	
+	private int dayKWH(){
+		int avgKWHperYear = 14000;
+		int dateKWH = avgKWHperYear/365;
+		return dateKWH;
 		
+	}
+	
+	private int hourKWH(){
+		int avgKWHperYear = 14000;
+		int avgKWHperDay = avgKWHperYear/365;
+		int hourKWH = avgKWHperDay/24;
+		return hourKWH;
 	}
 
 	/**
 	 * @param buttonID
 	 */
 	public void updateGraph(int buttonID) {
-		
-		if (buttonID == 1){
+
+		if (buttonID == DAY) {
 			timePeriod = DAY;
-		} else if (buttonID == 2){
+		} else if (buttonID == WEEK) {
 			timePeriod = WEEK;
-		} else if (buttonID == 4){
-			timePeriod = YEAR;
-		} else {
+		} else if (buttonID == MONTH) {
 			timePeriod = MONTH;
+		} else if (buttonID == YEAR) {
+			timePeriod = YEAR;
 		}
 
 		((ViewGroup) view).removeAllViews();
@@ -207,14 +244,12 @@ public class GraphFragment extends Fragment {
 		GraphicalView graph = ChartFactory.getBarChartView(getActivity(),
 				getGeneratedDataset(timePeriod), getChartRenderer(timePeriod),
 				Type.DEFAULT);
-		
+
 		graphLayout = (LinearLayout) view
 				.findViewById(R.id.graph_fragment_layout);
-		
-		((ViewGroup) view).addView(graph, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.MATCH_PARENT));
-		
-		
+
+		((ViewGroup) view).addView(graph, new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
 	}
 }
